4e7c06a44fb3cf2c1d623c73c182a52749b8dddf
unknown <farahkhanu@gmail.com>
2018-09-12 16:34:08 -0700

    Product routes completed, react dependencies on client side installed

--- server/server.js
+++ server/server.js
@@ -38,6 +38,43 @@ app.post('/api/product/article',auth,admin,(req,res)=>{
     })
 })
 
+app.get('/api/product/articles',(req,res)=>{
+    let order = req.query.order ? req.query.order : 'asc';
+    let sortBy = req.query.sortBy ? req.query.sortBy : '_id';
+    let limit = req.query.limit ? parseInt(req.query.limit) : 100;
+
+    Product.
+    find().
+    populate('brand').
+    populate('wood').
+    sort([[sortBy,order]]).
+    limit(limit).
+    exec((err,articles)=>{
+        if(err) return res.status(400).send(err);
+        return res.send(articles);
+    })
+
+})
+
+app.get('/api/product/articles_by_id',(req,res)=>{
+    let type = req.query.type;
+    let items = req.query.id;
+
+    if (type==="array"){
+        let ids = req.query.id.split(",");
+        items = [];
+        items = ids.map(item =>{
+            return mongoose.Types.ObjectId(item)
+        })
+    }
+    Product.
+    find({'_id':{$in:items}}).
+    populate('brand').
+    populate('wood').
+    exec((err,docs)=>{
+        return res.status(200).send(docs)
+    })
+});
 //============================================================
                         // WOODS
 //============================================================
