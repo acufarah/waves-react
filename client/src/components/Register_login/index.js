ccd07012a7ded3ba8beb3afad326288538a54a60
unknown <farahkhanu@gmail.com>
2018-09-17 22:23:51 -0700

    finished login view

--- client/src/components/Register_login/index.js
+++ client/src/components/Register_login/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import MyButton from '../utils/button';
+import Login from './login';
 
 const RegisterLogin = () =>{
     return(
@@ -26,7 +27,7 @@ const RegisterLogin = () =>{
                     <div className="right">
                         <h2>Registered Customers</h2>
                         <p>If you have an account, please login.</p>
-                        LOGIN
+                        <Login/>
                     </div>
                 </div>
             </div>
